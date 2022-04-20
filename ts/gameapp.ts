c6335e08c433c9703d3bedfebf1df54100d303da
JakobNeesgaard <jn_unforgiven@yahoo.dk>
2022-04-21 00:06:06 +0200

    Initial commit

---
+++ ts/gameapp.ts
@@ -0,0 +1,26 @@
+
+//import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database 
+//const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+
+
+
+console.log('start2!');
+//alert('fsdf2');
+
+
+
+document.addEventListener('DOMContentLoaded', run, false);
+
+
+
+function run() {
+
+  alert('test31');
+
+
+
+    
+}
+
